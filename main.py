580aabfb770db2a90d9a1dbc79d4fca670cd990c
codingninja2 <edwinafatsawo@gmail.com>
2024-12-13 20:10:10 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Storing Values
+Math = 89
+English = 91
+Science = 92
+RME = 86
+French = 72
+
+# Finding the total of trees
+sum = Math+English+Science+RME+French
+print("the sum of all the 5 Subjects is: ", sum)
+
+# Finding the average of trees
+average = sum/5
+print("the average of all the Subjects is: ", average)
